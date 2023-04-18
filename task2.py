d29f2341394dbf561c29d292d1422fe8057832e9
Denis Abrosimov <bggth.studio@gmail.com>
2023-04-18 12:58:21 +0300

    add: task 2.3

--- task2.py
+++ task2.py
@@ -39,12 +39,11 @@ def task_21():
 	for x in result.fetchall():
 		print(x)
 
-def task_22():
+def task_22_23():
 	print('\ntask 2.2 table')
 	cur = con.cursor()
 	try:
 		cur.execute('DROP TABLE table1')
-		cur.execute('DROP TABLE table2')
 	except:
 		pass
 		
@@ -75,23 +74,18 @@ def task_22():
 		print(x)
 
 	print('\ntask 2.2 result')
-	result = cur.execute('''SELECT CatalogID, SUM(PositionQuantity * PositionPrice) FROM table1 WHERE RequestID = 111 GROUP BY CatalogID''')
+	result = cur.execute('SELECT CatalogID, SUM(PositionQuantity * PositionPrice) FROM table1 WHERE RequestID = 111 GROUP BY CatalogID')
 	for x in result.fetchall():
 		print(x)
 
 	print('\ntask 2.3 result')
-	#result = cur.execute('''SELECT CatalogID, sum FROM (SELECT CatalogID, SUM(PositionQuantity * PositionPrice) as sum FROM table1 WHERE RequestID = 111 GROUP BY CatalogID) WHERE sum > 100000''')
-	result = cur.execute('''SELECT CatalogID, SUM(PositionQuantity * PositionPrice) as sum FROM table1 WHERE RequestID = 111 GROUP BY CatalogID''')
+	result = cur.execute('SELECT CatalogID, SUM(PositionQuantity * PositionPrice) as sum FROM table1 WHERE RequestID = 111 GROUP BY CatalogID HAVING (sum > 100000)')
 	for x in result.fetchall():
 		print(x)
 
-def task_23():
-	pass
-
 def main():
 	task_21()
-	task_22()
-	task_23()
+	task_22_23()
 
 if __name__ == '__main__':
 	main()
